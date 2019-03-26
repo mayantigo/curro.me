92b9b63bb6f5c866874a5f96a5cbc6c8b1f029a7
francisco.gonzalez <francisco.gonzalez@barceloviajes.com>
2019-03-26 14:49:45 +0100

    - Fix aphrodite implementation. - Renamed to index.jsx. - Removed unused text.

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -15,7 +15,7 @@ export default function Photo({ url }: PhotoType) {
           Photo: {
             backgroundColor: 'blue',
           },
-        }),
+        }).Photo,
       )}
       src={url}
       alt="Profile"
